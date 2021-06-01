17e8272fee8f61c0e16814c3b75baa4390f32f99
Marx <marxyng01@gmail.com>
2021-05-31 23:19:31 -0500

    added interactivity

---
+++ app.js
@@ -0,0 +1,6 @@
+let changeThings = () => {
+    //let element =document.getElementById("bN1");
+   
+    document.body.style.backgroundColor= "#000000";
+    document.querySelector("#bN1").innerHTML="You clicked me";
+}
